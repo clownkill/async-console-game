dc072c9aabb866ba61be415bb1b56e68aa5eb570
clownkill <alehnovich90@yandex.ru>
2022-06-10 10:56:36 +0300

    add asinc star

--- main.py
+++ main.py
@@ -1,26 +1,36 @@
+import asyncio
 import time
 import curses
 
 
+async def blink(canvas, row, column, symbol='*'):
+    while True:
+        canvas.addstr(row, column, symbol, curses.A_DIM)
+        await asyncio.sleep(0)
+
+        canvas.addstr(row, column, symbol)
+        await asyncio.sleep(0)
+
+        canvas.addstr(row, column, symbol, curses.A_BOLD)
+        await asyncio.sleep(0)
+
+        canvas.addstr(row, column, symbol)
+        await asyncio.sleep(0)
+
+
 def draw(canvas):
     row, column = (5, 20)
+    star_blink = blink(canvas, row, column)
+    curses.curs_set(False)
+    canvas.border()
     while True:
-        curses.curs_set(False)
-        canvas.border()
-        canvas.refresh()
-        canvas.addstr(row, column, '*', curses.A_DIM)
-        time.sleep(2)
-        canvas.refresh()
-        canvas.addstr(row, column, '*')
-        time.sleep(0.3)
-        canvas.refresh()
-        canvas.addstr(row, column, '*', curses.A_BOLD)
-        time.sleep(0.5)
-        canvas.refresh()
-        canvas.addstr(row, column, '*')
-        time.sleep(0.3)
-        canvas.refresh()
-
+        try:
+            star_blink.send(None)
+            canvas.refresh()
+        except StopIteration:
+            break
+    time.sleep(5)
+    
 
 if __name__ == '__main__':
     curses.update_lines_cols()
