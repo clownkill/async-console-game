8174556aac8b12006a9d75d6adfd938fac516bf5
clownkill <alehnovich90@yandex.ru>
2022-06-10 11:11:17 +0300

    add iteration for courutine

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import asyncio
 import time
 import curses
+from types import coroutine
 
 
 async def blink(canvas, row, column, symbol='*'):
@@ -19,17 +20,35 @@ async def blink(canvas, row, column, symbol='*'):
 
 
 def draw(canvas):
-    row, column = (5, 20)
-    star_blink = blink(canvas, row, column)
+    # row, column = (5, 20)
+    star1_blink = blink(canvas, row=5, column=20)
+    star2_blink = blink(canvas, row=5, column=25)
+    star3_blink = blink(canvas, row=5, column=30)
+    star4_blink = blink(canvas, row=5, column=35)
+    star5_blink = blink(canvas, row=5, column=40)
+
+    coroutines = [
+        star1_blink,
+        star2_blink,
+        star3_blink,
+        star4_blink,
+        star5_blink,
+        ]
+
     curses.curs_set(False)
     canvas.border()
+
     while True:
-        try:
-            star_blink.send(None)
+        for coroutine in coroutines:
             canvas.refresh()
-        except StopIteration:
+            try:
+                coroutine.send(None)
+            except StopIteration:
+                coroutines.reverse(coroutine)
+        if len(coroutines) == 0:
             break
-    time.sleep(5)
+
+    time.sleep(1)
     
 
 if __name__ == '__main__':
