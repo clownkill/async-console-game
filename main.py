4b3388de7c950abae2f5975f31223aaa48a4d723
clownkill <alehnovich90@yandex.ru>
2022-06-13 14:31:36 +0300

    add fire animation

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import curses
 from random import choice, randint
 import time
 
+from fire_animation import fire
+
 
 STARS = '+*.:'
 
@@ -31,6 +33,8 @@ def draw(canvas):
     x, y = canvas.getmaxyx()
     coroutines = []
 
+    fire_coroutine = fire(canvas, start_row=x-2, start_column=y//2)
+
     for _ in range(100):
         coroutines.append(blink(canvas, row=randint(1, x - 2), column=randint(1, y - 2), symbol=choice(STARS)))
 
@@ -38,10 +42,20 @@ def draw(canvas):
     canvas.border()
     
     while True:
+
         for coroutine in coroutines:
             coroutine.send(None)
         canvas.refresh()
+
+        try:
+            fire_coroutine.send(None)
+        except StopIteration:
+            fire_coroutine = fire(canvas, start_row=x-2, start_column=y//2)
+
+        canvas.refresh()
         time.sleep(0.1)
+
+        
     
 
 if __name__ == '__main__':
