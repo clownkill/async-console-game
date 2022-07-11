01b708cbe1bb6e6fd0d95e99274c013ae6e2a4a2
Evgeniy Alehnovich <alehnovich90@yandex.ru>
2022-07-11 19:52:02 +0300

    Merge branch 'main' of E:\PythonProject\async-console-game with conflicts.

--- main.py
+++ main.py
@@ -1,47 +1,54 @@
 import asyncio
-import curses
-from random import choice, randint
 import time
-
-
-STARS = '+*.:'
+import curses
+from types import coroutine
 
 
 async def blink(canvas, row, column, symbol='*'):
     while True:
         canvas.addstr(row, column, symbol, curses.A_DIM)
-        for _ in range(randint(1, 20)):
-            await asyncio.sleep(0)
-        
+        await asyncio.sleep(0)
+
         canvas.addstr(row, column, symbol)
-        for _ in range(randint(1, 20)):
-            await asyncio.sleep(0)
-       
+        await asyncio.sleep(0)
+
         canvas.addstr(row, column, symbol, curses.A_BOLD)
-        for _ in range(randint(1, 20)):
-            await asyncio.sleep(0)
-       
+        await asyncio.sleep(0)
+
         canvas.addstr(row, column, symbol)
-        for _ in range(randint(1, 20)):
-            await asyncio.sleep(0)
-        
+        await asyncio.sleep(0)
 
 
 def draw(canvas):
-    x, y = canvas.getmaxyx()
-    coroutines = []
+    # row, column = (5, 20)
+    star1_blink = blink(canvas, row=5, column=20)
+    star2_blink = blink(canvas, row=5, column=25)
+    star3_blink = blink(canvas, row=5, column=30)
+    star4_blink = blink(canvas, row=5, column=35)
+    star5_blink = blink(canvas, row=5, column=40)
 
-    for _ in range(100):
-        coroutines.append(blink(canvas, row=randint(1, x - 2), column=randint(1, y - 2), symbol=choice(STARS)))
+    coroutines = [
+        star1_blink,
+        star2_blink,
+        star3_blink,
+        star4_blink,
+        star5_blink,
+        ]
 
     curses.curs_set(False)
     canvas.border()
-    
+
     while True:
         for coroutine in coroutines:
-            coroutine.send(None)
-        canvas.refresh()
-        time.sleep(0.1)
+            canvas.refresh()
+            try:
+                coroutine.send(None)
+            except StopIteration:
+                coroutines.reverse(coroutine)
+        if len(coroutines) == 0:
+            break
+
+    time.sleep(1)
     
 
 if __name__ == '__main__':
